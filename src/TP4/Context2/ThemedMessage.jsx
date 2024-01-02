f5cae0cb68fa3fbf5859abcc62a4aa84b19c0668
Oussalem-zakaria <zakaryaoussalem@gmail.com>
2024-01-02 15:43:21 +0100

    Update ThemedMessage component styling

--- src/TP4/Context2/ThemedMessage.jsx
+++ src/TP4/Context2/ThemedMessage.jsx
@@ -5,7 +5,7 @@ function ThemedMessage() {
 
     return ( 
         <>
-            <p style={{backgroundColor: theme === "light" ? "#fff" : "#111",color: theme === "light" ? "#111" : "#FFF"}} className={`py-5 px-4 border-2 border-black rounded-lg shadow-lg`}>
+            <p style={{backgroundColor: theme === "light" ? "#fff" : "#111",color: theme === "light" ? "#111" : "#FFF"}} className={`py-5 px-4 border-2 border-black rounded-lg shadow-lg text-center w-72 h-72`}>
                 This is a thimed message
             </p>
         </>
